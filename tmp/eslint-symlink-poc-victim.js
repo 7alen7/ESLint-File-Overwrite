9113916762d4ae3bc457798c75c803b7ad6b8ffd
Dan <71292133+7alen7@users.noreply.github.com>
2026-05-14 15:04:31 -0500

    Create eslint-symlink-poc-victim.js

---
+++ tmp/eslint-symlink-poc-victim.js
@@ -0,0 +1,3 @@
+ // VICTIM FILE — sits OUTSIDE the project being linted. Currently in the /tmp folder for demonstration purposes.
+  const secret = 'do-not-modify'
+  const other  = 'also-do-not-modify'
